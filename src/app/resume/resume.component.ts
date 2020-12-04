990613e9c324f72edbaf613ff4b55a4aec244af8
AMROAMRO404 <amro.amro1999@gmail.com>
2020-12-04 21:02:36 +0200

    generate pdf copy

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-resume',
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.css']
 })
-export class ResumeComponent implements OnInit {
+export class ResumeComponent {
 
-  constructor() { }
+  generatePDF() {
+    var data = document.getElementById('htmlData');
+    html2canvas(data).then(canvas => {
 
-  ngOnInit(): void {
+      // Few necessary setting options  
+      var imgWidth = 208;
+      var pageHeight = 295;
+      var imgHeight = canvas.height * imgWidth / canvas.width;
+      var heightLeft = imgHeight;
+      const contentDataURL = canvas.toDataURL('image/png')
+      let pdf = new jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+      var position = 0;
+
+      pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+      pdf.html(document.body)
+      pdf.save('CV.pdf');
+
+    });
+
   }
-
 }
+
