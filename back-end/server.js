982144e8298a13eb9920b68db680b4dc4d67ff92
AMROAMRO404 <amro.amro1999@gmail.com>
2020-12-07 20:08:43 +0200

    last try to deploy :(

--- back-end/server.js
+++ back-end/server.js
@@ -1,12 +1,10 @@
 const http = require('http');
 const app = require('./app');
 
-const port = Process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port, () => {
-    console.log("server listening to port " + port);
-});
+server.listen(port);
 
 console.log('Listening on 3000...');
