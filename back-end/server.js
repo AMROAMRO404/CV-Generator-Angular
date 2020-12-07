406a2669ce93f781adef5b9812d9de1faad07426
AMROAMRO404 <amro.amro1999@gmail.com>
2020-12-07 18:57:12 +0200

    fix issue in server

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,12 @@
 const http = require('http');
 const app = require('./app');
 
-const port = 3000;
+const port = Process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
-server.listen(port);
+server.listen(port, () => {
+    console.log("server listening to port " + port);
+});
 
 console.log('Listening on 3000...');
